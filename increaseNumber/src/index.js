b2102afd43b22a009fca502402348570d115a23e
Puja Sah <pujasah01.1998@gmail.com>
2021-04-18 20:01:28 +0530

    completed

--- increaseNumber/src/index.js
+++ increaseNumber/src/index.js
@@ -1,10 +1,15 @@
 // A number increased by 7 equals 22. Write a function that return A number's value.
 //  Return null if initial_value or final_value is not a number and when final_value is less than initial_value.
 
-
 // Write the function to find the increaseNumber obtained.
 function increaseNumber(initialValue, finalValue) {
-     //your code here
+  if (typeof initialValue !== "number" && typeof finalValue !== "number") {
+    return null;
+  } else if (finalValue < initialValue) {
+    return null;
+  } else {
+    return finalValue - initialValue;
   }
+}
 
 //Use SpecRunner to check the Test Cases.
